f76bfe2d456f604136c488adf744f32796cf3213
Satthi654 <Satthi654@gmail.com>
2023-03-17 15:51:18 +0530

    Updated Test.java

--- Test.java
+++ Test.java
@@ -3,4 +3,12 @@ public class Test{
 	public static void main(String[] args){
 		
 	}
+	
+	public void printHelloWorld(){
+		System.out.println("Hello World!, We Are Going to rock in Java");
+		System.out.println("Hello World!, We Are Going to rock in Java");
+		System.out.println("Hello World!, We Are Going to rock in Java");
+		System.out.println("Hello World!, We Are Going to rock in Java");
+		
+	}
 }
