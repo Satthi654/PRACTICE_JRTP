bd6e11092893139ead9d70aca48b11165d271973
Satthi654 <Satthi654@gmail.com>
2023-03-17 16:05:44 +0530

    Test.java updated

--- Test.java
+++ Test.java
@@ -3,13 +3,20 @@ public class Test{
 	public static void main(String[] args){
 		
 	}
-	
+	int a , b;
+	int sum =0;
 	public void performAddition(){
-		int a , b;
-		int sum =0;
 		a = 10;
 		b = 20;
 		sum = 10 + 20;
 		System.out.println("The sum of "+a+" and "+b+" are : "sum);
 	}
+	int diff;
+	public void performSubtraction(){
+		a = 20;
+		b = 10;
+		diff = a - b;
+		System.out.println("The Difference  Between  "+a+" and "+b+" are : "diff);
+	}
+	
 }
